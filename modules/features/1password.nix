41566100805275e3d7fde84c78363b7968074ac9
Justin Conner <jstncnnr@outlook.com>
2026-04-08 19:38:56 -0600

    add ssh identity agent for 1Password on darwin

--- modules/features/1password.nix
+++ modules/features/1password.nix
@@ -29,7 +29,11 @@
         { pkgs, ... }:
         {
           programs.ssh.matchBlocks."*" = {
-            identityAgent = if pkgs.stdenv.isLinux then "~/.1password/agent.sock" else "";
+            identityAgent =
+              if pkgs.stdenv.isLinux then
+                "~/.1password/agent.sock"
+              else
+                "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
           };
         };
 
